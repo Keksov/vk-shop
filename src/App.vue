59402f48c74fb8a1bfd4c3c29b7402f541a9c8bb
Timures <timures@mail.ru>
2021-03-20 01:22:58 +0600

    add clipboard2, add ava img, find bag

--- src/App.vue
+++ src/App.vue
@@ -17,62 +17,83 @@ export default {
     return {
       products: [
         {
-            "seller":{
-              "name":"Габриелла Мельник",
-              "vk":"https://vk.com/id286389627",
-              "vkid":286389627
-            },
-            "id":4872,
-            "date":1615825502,
-            "price":3900,
-            "vk":"https://vk.com/wall286389627_4872",
-            "category":"Прочее",
-            "text":"Цена: 3900\nNew collection 2021\nШикарные куртки эко—кожа—рукава трикотаж инкрустированные камнями фото не передаёт всю красоту—неотразимый блеск камней\nШикарнейшее качество\nРазмеры s, m, l, xl\nЦена: 3900₽",
-            "photos":[
-              "https://sun9-73.userapi.com/impf/xJYAtW1TyJlljYHaCrC6WSgEkGLgGsRp-1pSow/-L9cYD6YBiw.jpg?size=605x807&quality=96&sign=94aea35d061d6e0c447ba177e8a37194&c_uniq_tag=rpx3AQ7p3_mSabTyJLXo2WVrNrCylTWollnNnca1ydo&type=album",
-              "https://sun9-25.userapi.com/impf/KoBUUjcYO1ypwTwVFKc0GsNhBwuKC38g7afIWA/9riCqW5w16o.jpg?size=605x807&quality=96&sign=2460052069761630e8a704fb46d5bf9f&c_uniq_tag=wj_rc3U5Z7a2dgLcfK98d0GjHTWsbe84Zh3DmmGuWrA&type=album",
-              "https://sun9-27.userapi.com/impf/EvJ-G8TnkH6aHU--vZPeXEXeP-swSiciWRZDVw/Wu95pQCuH6E.jpg?size=605x807&quality=96&sign=677c7194a3566c2ba90d0dbbab38d6cd&c_uniq_tag=OhfBVnwrZ-1ye8Pl1B3nHPvNo7BHIee1maucaSw_Q60&type=album",
-              "https://sun9-61.userapi.com/impf/mqJ8AgU5RNwG1DLrQbldm7hxCRl4Rn7zuz7jbw/3Ph-TNqHYo8.jpg?size=605x807&quality=96&sign=75c661a761cf645e4bebc19be2ac4fe9&c_uniq_tag=4YZ8ia4IyKMXVvr2OPoE9ZYFzhjcb5v_WR0tuOGLQ-E&type=album"
-            ]
+        "seller": {
+            "name": "Муслим Абдуллоев",
+            "vk": "https://vk.com/id535401436",
+            "vkid": 535401436,
+            "vkav": "https://sun1-25.userapi.com/s/v1/ig1/kkCYcPyBuAM9_1gAoocb658s7UTpW_6WHP-vA1NmGmVht8UA0m3biYSV0JWTfmHiNSDNBr-P.jpg?size=50x0&quality=96&crop=0,0,799,799&ava=1"
         },
-        {
-            "seller":{
-              "name":"Габриелла Мельник",
-              "vk":"https://vk.com/id286389627",
-              "vkid":286389627
-            },
-            "id":4871,
-            "date":1615825426,
-            "price":3500,
-            "vk":"https://vk.com/wall286389627_4871",
-            "category":"Прочее",
-            "text":"Цена: 3500\nNew collection 2021\nШикарные комбинированные куртки—карманы инкрустированные камнями фото не передаёт всю красоту—неотразимый блеск камней\nШикарнейшее качество\nРазмеры s, m, l, xl\nЦена: 3500₽",
-            "photos":[
-              "https://sun9-34.userapi.com/impf/BVj2XpmJvF-5hsjDBASirLoC00en0cD7WSQX0g/tO0ssFT_CvY.jpg?size=605x807&quality=96&sign=a96f2c7296edd1822c6ca55ebefcdac7&c_uniq_tag=ZHpjOgi3YXbCxNJ7HF5aRgG5eKuvAHQ-xrCYBOVN0JA&type=album",
-              "https://sun1-88.userapi.com/impf/RAcmWhPMif5ONoIAUGWOSGiEtUdVN5UO66mJUg/mCLtQHm4eu4.jpg?size=605x807&quality=96&sign=1ed6952d93d1d9135fed7af19becc2c9&c_uniq_tag=lXNZU97wMKvzii3F2plg6BHi2hKd3-usqMILt3b5GlI&type=album",
-              "https://sun9-3.userapi.com/impf/R50SbkCQi9I3HUXbGaYYiqEeKQVuz-BUOi4Y6A/zxXMiog6fE4.jpg?size=605x807&quality=96&sign=a1329c3396eb89e8143f0cb151bbf39c&c_uniq_tag=z6-QKyG3hOzYRofhzONFq7nCp-5zYlAzW_WnyknvoQ4&type=album",
-              "https://sun9-6.userapi.com/impf/UAs1gjbvaitL6s7eyU8XiXs2Ci582itNRoXKTA/X818VWxd8AA.jpg?size=605x807&quality=96&sign=9c6b7fad5998b6ff8bcd75d6b3648aa1&c_uniq_tag=JAOKroDXFOjamY46swjTpjlC6fCbFgSUHz3cqyNsPEY&type=album"
-            ]
+        "id": 10898,
+        "date": 1616102130,
+        "price": "",
+        "vk": "https://vk.com/wall535401436_10898",
+        "category": "Женская одежда",
+        "text": "Внимание! внимание! супер распродажа по 100 р размер: 42-44-46-48-50-52-54-56-58-60\nМодел и цвета без выбора @ ‍ ‍ ‍",
+        "photos": [
+            "https://sun9-18.userapi.com/impg/nmr-nckcQQvXmarClDknk4xQ-YMcm5M6kz6fGg/OY8LeS5rBmE.jpg?size=612x807&quality=96&sign=ce17a24b03352b734a797d4705f62228&c_uniq_tag=vN0xU4BBAf1k-eEz_VFquC0XmMeBVxUpL5JRVDdxljw&type=album",
+            "https://sun9-45.userapi.com/impg/S4QergzjQHWjuYXWrZQrmQ9IPVWPd4zZHg5DuQ/rFzrh7kHf0k.jpg?size=578x604&quality=96&sign=6788d643c36b87012858ada49e59a66b&c_uniq_tag=o5Ajxj0_aPwZftTFdDIYgYHggCwrMMCFNGKKJTWoTCM&type=album",
+            "https://sun9-15.userapi.com/impg/gqa8MZ2hEzKA9UtHcpeTVJlkfLXeqiNMjxUUJQ/jbGgczpVXCg.jpg?size=582x604&quality=96&sign=c204d5076ab8c50e1bf6e554af0b9e12&c_uniq_tag=u7L7LGDVooMUi_Xy7ps_eO9dgPYRCqkXPsCStrJgbT8&type=album",
+            "https://sun9-76.userapi.com/impg/pmcIM22Wzw7slM1Gpr2J-25lhZJCMKhxhmgynQ/WH1XSytjFcE.jpg?size=552x604&quality=96&sign=4eb9aec54a2953de9d3e6cab508d0337&c_uniq_tag=jfLPQiImtrU6vUm3fP2tWvmRI8qrLT0BtFK5deTnhjA&type=album"
+        ]
+    },
+    {
+        "seller": {
+            "name": "Фёдор Халиков",
+            "vk": "https://vk.com/id546469140",
+            "vkid": 546469140,
+            "vkav": "https://sun1-89.userapi.com/s/v1/if1/BOr7fDdd72Fc4dGHoaQDctnNRncl7wJHu9iXteTsR47hM0Ha2m0gSOhrAG8zg237ZuwEeViR.jpg?size=50x0&quality=96&crop=0,114,1052,1052&ava=1"
         },
-        {
-            "seller":{
-              "name":"Габриелла Мельник",
-              "vk":"https://vk.com/id286389627",
-              "vkid":286389627
-            },
-            "id":4870,
-            "date":1615825349,
-            "price":4200,
-            "vk":"https://vk.com/wall286389627_4870",
-            "category":"Прочее",
-            "text":"Цена: 4200\nNew collection 2021\nШикарные куртки—карманы инкрустированные камнями фото не передаёт всю красоту—неотразимый блеск камней\nШикарнейшее качество\nРазмеры s, m, l, xl\nЦена: 4200₽",
-            "photos":[
-              "https://sun9-58.userapi.com/impf/ncwRaYQI7Opw7Nt45o4uI1slVrUwFzO2JpcJgA/lcrlK6K05fg.jpg?size=605x807&quality=96&sign=64cf9c7d01a56f26122d76692b447681&c_uniq_tag=4MbpGDikc9ADG7ZUfiDlUfzck8M4CIMSo3Ol1H_6Qr0&type=album",
-              "https://sun9-27.userapi.com/impf/CIO2-8qhDHJD2SzBzsaidKlCC4jbkFihjSoY2A/yAhYz1mpQfc.jpg?size=605x807&quality=96&sign=bfe0fc5c44f5b69309a863ee1258120a&c_uniq_tag=psAwre1FcBMgCAqgOOVoeaWRD7v1m9_1qksYwSmhSqM&type=album",
-              "https://sun1-26.userapi.com/impf/ZiFq__ytypntL1NASknB-1a0dZi4nh4ISFYENQ/DU5k9nznAgU.jpg?size=605x807&quality=96&sign=2e4723488ba6d26ab77215eb6d2a1d3c&c_uniq_tag=sPJYeyYp9T6sm9nhA68dMONyoDmJ-uBkDUgU6g0HV00&type=album",
-              "https://sun1-91.userapi.com/impf/ZMJ8ljeEJu66i9Jep3cQnNK0unUWvypwTcxIkw/rVvpIASyW4c.jpg?size=605x807&quality=96&sign=c40464bac15f8e2cdf984d9ff9269d6f&c_uniq_tag=YybZKLhvfeDoQoEdkOW7vkbV41bxAKZYziwiw4jmJ-I&type=album"
-            ]
+        "id": 5179,
+        "date": 1616102066,
+        "price": 700,
+        "vk": "https://vk.com/wall546469140_5179",
+        "category": "Женская одежда",
+        "text": "Цена: 700\nСнова получил платье выполнена\nИзготовленно из плотного трикотажа.очень хорошее качество, аккуратный пошив.цена: 700 руб\nРазмеры 54-56-58-60-62-64\nЕсть в наличии 22-108\nhttps://vk.com/club192488612",
+        "photos": [
+            "https://sun9-35.userapi.com/impg/X7gODtIzEa-urGdK_oZlx7v56b2JRmfxW3FPKw/7PFNnQ2uxXY.jpg?size=600x604&quality=96&sign=d06c66ed0cddecf27e0925016d6799cb&c_uniq_tag=41YJ5eA6aJsdQtfhttrH4wDppxEokoLNctXH7XvCrjI&type=album",
+            "https://sun9-51.userapi.com/impg/wTfwLM12t5TCIfSoMZoXYPSk-LUlZ9KiBZTzKQ/B-S4LaMf8JM.jpg?size=604x601&quality=96&sign=2306bc999c939c01473f2e118115c5ef&c_uniq_tag=8N7k3AidzKgHh9mxW8G3ctT7F5G061N95edDQuVgTrk&type=album"
+        ]
+    },
+    {
+        "seller": {
+            "name": "Тагоев Хожиакбар",
+            "vk": "https://vk.com/id551538922",
+            "vkid": 551538922,
+            "vkav": "https://sun1-89.userapi.com/s/v1/ig2/qXWOAqJaK8nAiEVjlRzMyZUn_-k49Vh3ov9qzHoA9vF4QZGddSKoZq_XMqlAdqq1Pml7IJgKAuRBlIVrd8epvJc0.jpg?size=50x0&quality=96&crop=4,0,819,819&ava=1"
         },
+        "id": 47896,
+        "date": 1616102006,
+        "price": 900,
+        "vk": "https://vk.com/wall551538922_47896",
+        "category": "Обувь (штучно)",
+        "text": "Цена: 900\nСупер скидка\n2021 весна\nЛюксовое качество！\nРазмер: 36-40-38×2\nКоробка: 6 пар\nЦена: 900₽\nРеальное фото\nРазмер в размер\n",
+        "photos": [
+            "https://sun9-53.userapi.com/impg/1XcFWjcRWCOGgIVw6vILsSx4CVva2Qh2sQYxYA/m0G4leFTewo.jpg?size=605x807&quality=96&sign=d6c8a5c89aaa0137dec065a3e6973926&c_uniq_tag=2AojnKUN8K0SLvS2SkyCW_mkvSbCKAIAiv5qZe-Xx7M&type=album",
+            "https://sun1-27.userapi.com/impg/nSQaNrYewnKQ4Cloku6DIw_g6E8qTqxPu0wPQg/SHJdOzBI0dU.jpg?size=593x807&quality=96&sign=99ca0c3f6bbdcb0e4619ec5d5e1d1191&c_uniq_tag=jkJDigV52hKq2x5EM0k57EqqaHMHXdyk0il5M2lK-Wc&type=album",
+            "https://sun9-10.userapi.com/impg/LPgqQRqfl9PUaUBs-IlH2b2Pwp_jzp7QVs9LIQ/T9gYCnWKgDo.jpg?size=643x807&quality=96&sign=9576418f89ca5a39760a8c90dadabb7f&c_uniq_tag=8KoUeMNvyFq7kByUZ0rl-LXiw4whTbfZKZIFNEwVmpM&type=album",
+            "https://sun9-31.userapi.com/impg/JzutKBrc45MzPiVYkN0G5PDIyCUkhzTPu4IhUg/-0rfGxtXz38.jpg?size=591x807&quality=96&sign=18967b5cb5f4d23b8acef74a636287ba&c_uniq_tag=Pb7UBo6gROyWTjcwu16ONqeqh6yNStWmLYfO8JvEybM&type=album"
+        ]
+    },
+    {
+        "seller": {
+            "name": "Умар Мамарасулов",
+            "vk": "https://vk.com/id449009367",
+            "vkid": 449009367,
+            "vkav": "https://sun1-14.userapi.com/s/v1/ig2/PKZNR0CXtGgfgtuxDT3CcHwOzt9dgEyldJX4qFG0hiNtC0xBf2xIZKe3PL8RP6W4IVbvyceniL4lSLn6m0d2S8L8.jpg?size=50x0&quality=96&crop=0,269,1620,1620&ava=1"
+        },
+        "id": 46901,
+        "date": 1616102001,
+        "price": 250,
+        "vk": "https://vk.com/wall449009367_46901",
+        "category": "Обувь (штучно)",
+        "text": "Цена: 250\nВнимание!\nРаспродажа\nДетские кеды\nОптовая цена: 250₽\nРазмер: 26-31-12 пар\nВ коробке 2 расцветки\nКачество",
+        "photos": [
+            "https://sun9-61.userapi.com/impg/_slowZzK3EOgUrmA4AEwQdQ8RMsTLIITHWzuZg/bIlhagm-sVM.jpg?size=807x605&quality=96&sign=2c005d1033c7d1fa3b9c2938f12ecbfd&c_uniq_tag=fTKZYhTBuJtt45EZYLwFeUtloIag5_yO2XtxLmklEjI&type=album",
+            "https://sun9-33.userapi.com/impg/6RQtx_D8omwevrajZDsDjgXxUjV0X6Y1J_g_RA/gcJUAhDg7yE.jpg?size=605x807&quality=96&sign=5cb52368010cd82590de038ad9d0cfc7&c_uniq_tag=EhV1x4mqBhpFSriUNECN9dBaDPWPM-2x7tTrIk0-Jak&type=album",
+            "https://sun9-12.userapi.com/impg/u6BxTfh2f9vXADyLaX9cqoHmgX2kWluFRY-ibg/tREs4jaLjfM.jpg?size=605x807&quality=96&sign=2019f0de6cd980b79f4b6f5211211a1c&c_uniq_tag=BOKPqvwMExCd5QoTrqyK-xggQE-zrIyM0DtxwP2MYZs&type=album",
+            "https://sun9-36.userapi.com/impg/YRWyj-_5tFubxyVIYDq9Or7C0fVZH73C0HFBfg/ND2HrmgyQiY.jpg?size=807x605&quality=96&sign=66ec97148e9d982dee5121c99e15772b&c_uniq_tag=5VUhv35wvuEG2u2cJE2I-XqQhIVFwc2TUIP8RekHCXk&type=album"
+        ]
+    }
         
       ]
     }
--- src/components/ProductGallery.vue
+++ src/components/ProductGallery.vue
@@ -1,48 +1,151 @@
 <template>
     <div>
-        <!-- <div class="gallery-wrapper" @click="showProduct(product.id)">
+        <div class="gallery-wrapper" @click="showProduct()">
             
-        <div class="gallery-main" v-bind:class="{ bottom: isBottom }">            
-            <img :src="product.photos[0]" alt="main photo">            
-        </div>
-        <div v-bind:class="[isBottom ? 'gallery-bottom' : 'gallery-right']">
-            <img :src="product.photos[1]">
-            <img :src="product.photos[2]">
-            <img :src="product.photos[3]">
-        </div> -->
-        <!-- links -->
-        <div class="product-item" v-show="isProductItemShow">
-            <div class="product-item__top">{{ product.vk }}</div>
-            <div class="product-item__seller">
-                <div class="product-item__seller-name">{{ product.seller.name }}</div>
-                <div class="product-item__date">{{ currentDateTime(product.date) }}</div>
+            <div class="gallery-main" v-bind:class="{ bottom: isBottom }">            
+                <img :src="product.photos[0]" alt="main photo">            
             </div>
+            <div v-bind:class="[isBottom ? 'gallery-bottom' : 'gallery-right']">
+                <img :src="product.photos[1]">
+                <img :src="product.photos[2]">
+                <img :src="product.photos[3]">
+            </div>
+            <!-- links -->
         </div>
 
-        <!-- </div> -->
+        <transition name="slide" v-on:after-enter="afterEnter" v-on:after-leave="afterLeave">
+            <div class="product-info" :id="'2215'+product.id" v-show="isProductItemShow">
+                <div class="product-info__top">
+                    <div class="product-info__vk">
+                        <span>{{ product.vk }}</span>
+                    </div>
+                    
+                    <div class="product-info__seller">
+                        <div class="product-info__seller-ava">
+                            <img :src="product.seller.vkav" alt="ava">
+                        </div>
+                        <div class="product-info__seller-desc">
+                            <div class="product-info__seller-desc_name">{{ product.seller.name }}</div>
+                            <div class="product-info__seller-desc_date">{{ currentDateTime(product.date) }}</div>
+                        </div>
+                </div>
+                <div class="product-info__text">{{ product.text }}</div>
+                    
+                </div>
+                
+                <div class="product-info__photos">
+                    <!-- product links -->
+                    <div class="product-info__links">
+                        <div class="product-info__links-price" @click="goToProductTop()">
+                            <span class="product-info__links-price_value">{{ product.price }} р.</span> 
+                            <span class="product-info__links-price_plus">+</span>
+                        </div>
+                        <div class="product-info__links-share" @click="doCopy">
+                            <span class="icon share">
+                                <img src="@/assets/icons/share.svg" alt="share">
+                            </span>
+                        </div>
+                        <div class="product-info__links-favorite">
+                            <span class="icon favorite">
+                                <svg width="23" height="22" viewproductCardBox="0 0 23 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M12.2198 19.8624L11.8945 19.7461L11.5669 19.8553C8.65767 20.8253 6.66266 21.0603 5.30345 20.8596C4.01371 20.6692 3.28076 20.088 2.80473 19.2544C2.28996 18.3529 2.03963 17.0881 1.93937 15.5009C1.86585 14.337 1.87579 13.0933 1.88623 11.786C1.89 11.314 1.89384 10.8337 1.89384 10.3459C1.89384 8.67254 2.13229 7.50212 2.536 6.62747C2.93208 5.76934 3.5186 5.12764 4.33815 4.55813C5.18005 3.97309 6.24822 3.47667 7.60311 2.91034C7.97036 2.75683 8.3585 2.59826 8.76525 2.43208C9.71277 2.04498 10.7613 1.61661 11.8821 1.11486C13.0414 1.63664 14.1181 2.07835 15.0855 2.47522C15.4861 2.63956 15.8679 2.79621 16.2291 2.94768C17.5929 3.51954 18.6551 4.0146 19.4866 4.59681C20.2939 5.16217 20.8665 5.79888 21.2524 6.65774C21.6469 7.5357 21.8775 8.71524 21.8775 10.4104C21.8775 10.64 21.8778 10.8685 21.8782 11.0957C21.8808 12.6528 21.8832 14.1494 21.7639 15.52C21.6264 17.0991 21.3353 18.3746 20.79 19.2868C20.28 20.14 19.5224 20.7228 18.2583 20.9071C16.9288 21.101 14.9999 20.8563 12.2198 19.8624Z" stroke="#555555" stroke-width="2"/>
+                                </svg>
+                                <div class="favorite-dot"></div>
+                            </span>
+                        </div>
+                    </div>
+                    <!-- product photos -->
+                    <div class="product-info__gallery">
+                        <div class="product-info__photo" v-for="(photo, index) in product.photos" :key="index">
+                            <img :src="photo">
+                        </div>
+                    </div>
+
+                    
+                </div>
+
+                <div class="product-info__close" @click="closeProduct()">
+                    <span class="icon close">
+                        <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M21 1C14.6011 8.46535 8.21992 14.5823 1 21" stroke="#555555" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                        <path d="M21 21C13.5346 14.6011 7.41771 8.21992 1 1" stroke="#555555" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+                    </span>
+                </div>
+            </div>
+        </transition>
+        
     </div>
 </template>
 
 <script>
 import moment from 'moment'
+
 export default {
     name: 'ProductGallery',
     data(){
         return {
-           isProductItemShow: true
+           isProductItemShow: false,
+           currenPosition: null,
+           productLink: ''
         }
     },
     props: ['product', 'isBottom'],
   
     methods:{
-        showProduct(productID){
-            console.log('product id ', productID)
+        // product show
+        afterEnter: function () {            
+            window.scrollTo({top: 0, behavior: "smooth"})            
         },
+        afterLeave: function () {
+            window.scrollTo({top: this.currenPosition, behavior: "smooth"})            
+        },
+        showProduct(){
+            // window.scrollTo(0, 0);
+            /** get current position, when close use it to scroll back to it */            
+            var productCard = document.getElementById('2215'+this.product.id)
+            let productCardBox = productCard.getBoundingClientRect()
+            this.currenPosition = productCardBox.top + pageYOffset
+            
+            this.isProductItemShow = true
+            this.productLink = this.product.vk
+        },
+        closeProduct(){
+            this.isProductItemShow = false            
+        },
+        // date time convert
         currentDateTime(date) {
             moment.locale("ru")
             return moment(date).format('D MMM - h:mm')
-        }
+        },
+        // go top
+        goToProductTop(){
+            window.scrollTo({top: 0, behavior: "smooth"})
+        },
+        // copy to clipboard2
+        doCopy() {
+        this.$copyText(this.productLink).then(function (e) {
+        //   показать уведомления
+          console.log(e)
+        }, function (e) {
+          
+          console.log(e)
+        })
+      }
     }
 
 }
 </script>
+
+<style lang="scss" scoped>
+.slide-leave-active,
+.slide-enter-active {
+  transition: 0.75s;
+}
+.slide-enter {
+  transform: translate(100%, 0);
+}
+.slide-leave-to {
+  transform: translate(100%, 0);
+}
+</style>
