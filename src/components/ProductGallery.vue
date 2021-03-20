d67ee1f71578678de14af1426b1ff854333210ca
Timures <timures@mail.ru>
2021-03-20 19:24:03 +0600

    add notifications, correct product info links, add normalize css

--- src/components/ProductGallery.vue
+++ src/components/ProductGallery.vue
@@ -36,23 +36,22 @@
                 <div class="product-info__photos">
                     <!-- product links -->
                     <div class="product-info__links">
-                        <div class="product-info__links-price" @click="goToProductTop()">
+                        <button class="product-info__links-price" @click="goToProductTop()">
                             <span class="product-info__links-price_value">{{ product.price }} р.</span> 
                             <span class="product-info__links-price_plus">+</span>
-                        </div>
-                        <div class="product-info__links-share" @click="doCopy">
-                            <span class="icon share">
-                                <img src="@/assets/icons/share.svg" alt="share">
-                            </span>
-                        </div>
-                        <div class="product-info__links-favorite">
-                            <span class="icon favorite">
-                                <svg width="23" height="22" viewproductCardBox="0 0 23 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        </button>
+                        <button class="icon product-info__links-share"
+                            v-clipboard:copy="product.vk"
+                            v-clipboard:success="copySucceeded"
+                        >
+                            <img src="@/assets/icons/share.svg" alt="share">
+                        </button>
+                        <button class="icon favorite product-info__links-favorite">
+                            <svg width="23" height="22" viewproductCardBox="0 0 23 22" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M12.2198 19.8624L11.8945 19.7461L11.5669 19.8553C8.65767 20.8253 6.66266 21.0603 5.30345 20.8596C4.01371 20.6692 3.28076 20.088 2.80473 19.2544C2.28996 18.3529 2.03963 17.0881 1.93937 15.5009C1.86585 14.337 1.87579 13.0933 1.88623 11.786C1.89 11.314 1.89384 10.8337 1.89384 10.3459C1.89384 8.67254 2.13229 7.50212 2.536 6.62747C2.93208 5.76934 3.5186 5.12764 4.33815 4.55813C5.18005 3.97309 6.24822 3.47667 7.60311 2.91034C7.97036 2.75683 8.3585 2.59826 8.76525 2.43208C9.71277 2.04498 10.7613 1.61661 11.8821 1.11486C13.0414 1.63664 14.1181 2.07835 15.0855 2.47522C15.4861 2.63956 15.8679 2.79621 16.2291 2.94768C17.5929 3.51954 18.6551 4.0146 19.4866 4.59681C20.2939 5.16217 20.8665 5.79888 21.2524 6.65774C21.6469 7.5357 21.8775 8.71524 21.8775 10.4104C21.8775 10.64 21.8778 10.8685 21.8782 11.0957C21.8808 12.6528 21.8832 14.1494 21.7639 15.52C21.6264 17.0991 21.3353 18.3746 20.79 19.2868C20.28 20.14 19.5224 20.7228 18.2583 20.9071C16.9288 21.101 14.9999 20.8563 12.2198 19.8624Z" stroke="#555555" stroke-width="2"/>
                                 </svg>
-                                <div class="favorite-dot"></div>
-                            </span>
-                        </div>
+                            <div class="favorite-dot"></div>
+                        </button>
                     </div>
                     <!-- product photos -->
                     <div class="product-info__gallery">
@@ -64,21 +63,24 @@
                     
                 </div>
 
-                <div class="product-info__close" @click="closeProduct()">
-                    <span class="icon close">
-                        <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <button class="product-info__close icon close" @click="closeProduct()">
+                    <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M21 1C14.6011 8.46535 8.21992 14.5823 1 21" stroke="#555555" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
                         <path d="M21 21C13.5346 14.6011 7.41771 8.21992 1 1" stroke="#555555" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                        </svg>
-                    </span>
-                </div>
+                    </svg>
+                </button>
+                
             </div>
         </transition>
-        
+        <!-- notifications -->
+        <transition name="slide">
+            <div v-show="isCopied" class="notifications success">Скопировано</div>
+        </transition>
     </div>
 </template>
 
 <script>
+
 import moment from 'moment'
 
 export default {
@@ -87,7 +89,8 @@ export default {
         return {
            isProductItemShow: false,
            currenPosition: null,
-           productLink: ''
+           productLink: '',
+           isCopied: false
         }
     },
     props: ['product', 'isBottom'],
@@ -123,15 +126,11 @@ export default {
             window.scrollTo({top: 0, behavior: "smooth"})
         },
         // copy to clipboard2
-        doCopy() {
-        this.$copyText(this.productLink).then(function (e) {
-        //   показать уведомления
-          console.log(e)
-        }, function (e) {
-          
-          console.log(e)
-        })
-      }
+        copySucceeded(){
+            // show notif
+            this.isCopied = true
+            setTimeout(() => this.isCopied = false, 2500);
+        }
     }
 
 }
